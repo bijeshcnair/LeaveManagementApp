d0d280c8e07a3c6d2e73a41a95df2e851a61b588
Bijesh Chandran <bijesh@vanenburgsoftware.com>
2017-04-26 13:58:08 +0000

    Adding leave management logic New files added - [test] Files modified - [LeaveManagement,leftnav,MyLeaves,ApplyLeave,securityService,hrdb,data,project-security.xml,app.variables.json,pages-config.json]

--- src/main/webapp/pages/test/test.js
+++ src/main/webapp/pages/test/test.js
@@ -1,4 +1,4 @@
-Application.$controller("LeaveManagementPageController", ["$scope", function ($scope) {
+Application.$controller("testPageController", ["$scope", function ($scope) {
     "use strict";
 
     /* perform any action on widgets/variables within this block */
@@ -15,11 +15,3 @@ Application.$controller("LeaveManagementPageController", ["$scope", function ($s
     };
 
 }]);
-
-
-Application.$controller("grid1Controller", ["$scope",
-	function($scope) {
-		"use strict";
-		$scope.ctrlScope = $scope;
-	}
-]);
